2f3ee08ec72bc255598599ecf11a6bc9fa6c5d69
Pakjirayks <51689190+Pakjirayks@users.noreply.github.com>
2019-09-17 11:50:47 +0700

    Create helloController.php

---
+++ app/Http/Controllers/helloController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function showHello(){
+       return '<h1>Hello Controller : Pakjira Yaemkeson</h1>';
+    }
+}
