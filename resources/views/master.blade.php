1f7aaecb79f7e8eda15489d6eb60b580b6333101
Pakjirayks <51689190+Pakjirayks@users.noreply.github.com>
2019-10-01 11:59:48 +0700

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,1 +1,18 @@
 
+<html lang="en" dir="ltr">
+ <head>
+   <meta charset="utf-8">
+   <title>@yield('title')</title>
+   <style media="screen">
+     body{
+       background-color: yello;
+     }
+   </style>
+ </head>
+ <body>
+      @yield('content')
+ </body>
+ </footer>@yield('footer')</footer>
+</html>
+
+   
